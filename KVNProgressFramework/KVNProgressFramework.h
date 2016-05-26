4a1425b5fa781acb964ac40a11afbe5186f7b93c
Zoltan Bognar <z.bognar@aliceintokyo.net>
2016-05-26 15:35:57 +0200

    added support for carthage

---
+++ KVNProgressFramework/KVNProgressFramework.h
@@ -0,0 +1,24 @@
+//
+//  KVNProgressFramework.h
+//  KVNProgressFramework
+//
+//  Created by Zoltan Bognar on 26.05.16.
+//  Copyright © 2016 Pinch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVNProgressFramework.
+FOUNDATION_EXPORT double KVNProgressFrameworkVersionNumber;
+
+//! Project version string for KVNProgressFramework.
+FOUNDATION_EXPORT const unsigned char KVNProgressFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVNProgressFramework/PublicHeader.h>
+
+
+#import <KVNProgressFramework/UIColor+KVNContrast.>
+#import <KVNProgressFramework/UIImage+KVNEmpty.h>
+#import <KVNProgressFramework/UIImage+KVNImageEffects.h>
+#import <KVNProgressFramework/KVNProgress.h>
+#import <KVNProgressFramework/KVNProgressConfiguration.h>
